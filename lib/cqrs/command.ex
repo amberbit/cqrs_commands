3305d36cb46fd72ad99fe6ba5417a5e2f79f52eb
Hubert Łępicki <hubert.lepicki@amberbit.com>
2016-05-20 09:05:30 +0200

    add trigger() function

--- lib/cqrs/command.ex
+++ lib/cqrs/command.ex
@@ -2,6 +2,11 @@ defmodule Cqrs.Command do
   defmacro __using__(_opts) do
     quote do
       alias __MODULE__.Form
+
+      def trigger(event, payload) do
+        Cqrs.Events.Server.trigger event, payload \\ %{}
+      end
     end
   end
 end
+
