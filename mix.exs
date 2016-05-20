727d8d5eafa3ea50b18819976ddecf87eae8a200
Hubert Łępicki <hubert.lepicki@amberbit.com>
2016-05-20 11:06:49 +0200

    version bump

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Cqrs.Commands.Mixfile do
 
   def project do
     [app: :cqrs_commands,
-     version: "0.0.5",
+     version: "0.0.6",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
